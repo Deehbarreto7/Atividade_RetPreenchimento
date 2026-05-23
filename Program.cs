f5fd8ff8eab3d55cdd1a64812e93968655e82254
Débora <deehbarreto7@gmail.com>
2026-05-22 21:44:57 -0300

    atividade feita em sala de aula para fixação do conteúdo laços

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Qual a largura?" );
+int largura = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Qual a altura? ");
+int altura = Convert.ToInt32(Console.ReadLine());
+
+string preenchimento = "*";
+
+int contadorLinha = 0;
+while(contadorLinha < altura)
+{
+    int contadorColuna = 0;
+    while(contadorColuna < largura)
+    {
+         Console.Write(preenchimento);
+         contadorColuna = contadorColuna +1;
+    }
+    
+    Console.WriteLine();
+    contadorLinha = contadorLinha +1;
+}
